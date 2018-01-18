d93b321d7431c31c1b8919001ee8b6f1821781b7
brpthomas <pthoang6@gmail.com>
2018-01-17 21:03:00 -0500

    create new template, action, and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,7 @@
 class Article < ActiveRecord::Base 
     # add validation to maintain data integerity 
+    #this will require there are information is being inserted into the database
     validates :title, presence: true, length: {minimum: 3, maximum: 50}
     validates :description, presence: true, length: {minimum: 10, maximum: 300}
-
-    #this will require there are information is being inserted into the database
 
 end 
