9fbf76203d3a6256a74c1046e67ec33470d387c3
brpthomas <pthoang6@gmail.com>
2018-01-31 17:27:13 -0500

    add association between user and artciles and add debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ActiveRecord::Base 
+    belongs_to :user
     # add validation to maintain data integerity 
     #this will require there are information is being inserted into the database
     validates :title, presence: true, length: {minimum: 3, maximum: 50}
     validates :description, presence: true, length: {minimum: 10, maximum: 300}
-
+    validates :user_id, presence: true 
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+has_many :articles
+before_save { self.email = email.downcase }
 # adding validations to Users
 validates :username, presence: true, 
           uniqueness: { case_sensitive: false }, 
