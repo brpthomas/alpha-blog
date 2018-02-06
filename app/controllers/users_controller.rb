dc9a8f830b7ba69d42957ebad45cec89869bb436
brpthomas <pthoang6@gmail.com>
2018-02-05 19:20:23 -0500

    Addusers listing and update show artcile view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+    def index 
+        @users = User.all 
+    end 
+    
     def new
         @user = User.new
 
@@ -33,7 +38,7 @@ class UsersController < ApplicationController
 
     def show 
         @user = User.find(params[:id])
-        
+
     
     
     end  
