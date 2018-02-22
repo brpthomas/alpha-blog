f8c929fc7635100b92af0bdf1edb8d5bdf42a274
brpthomas <pthoang6@gmail.com>
2018-02-22 17:43:48 -0500

    updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,7 +65,7 @@ class UsersController < ApplicationController
 
     def require_admin
         if logged_in? and !current_user.admin? 
-            flash[:danger] = "Only admin users can perform that action"
+            flash[:danger] = "Only admin users can perform that action!"
             redirect_to root_path
         end
     end 
